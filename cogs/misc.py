8351aa80df9074ddfaa1b04d548ce46d98a63db7
Raj Dave <10864@bpsdoha.edu.qa>
2022-09-24 11:56:39 +0300

    create video search in misc.py

---
+++ cogs/misc.py
@@ -0,0 +1,83 @@
+import re, discord
+import yt_dlp
+from discord.ext import commands
+from youtube_search import YoutubeSearch
+from backend import log, embed_url, embed_footer, embed_header, embed_color, embed_icon
+
+
+class Other(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.ydl_opts = {
+            'format': 'bestaudio/best',
+            'noplaylist': True,
+            'quiet': True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]
+        }
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        log.info("Cogs : Misc.py Loaded")
+
+
+    @commands.slash_command(name="search", description="Searches for a song on YouTube.")
+    async def search(self, ctx, query):
+        await ctx.defer()
+
+        embed = discord.Embed(title=f"YouTube Search", url=embed_url,description=f"Here are your search results. Requested by {ctx.author.mention}",color=embed_color)
+        embed.set_author(name=embed_header, icon_url=embed_icon)
+
+        # If it not a URL, search for it
+        if not re.search(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?([^&=%\n]*)', query):
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            content = f"https://www.youtube.com/watch?v={results[0]['id']}"
+
+            log.debug(results[0])
+            log.debug(content)
+            # results[0] = {'id': 'id', 'thumbnails': ['https://i.ytimg.com/vi/id/hqdefault.jpg'], 'title': 'title', 'duration': 'duration', 'views': 'views', 'channel': 'channel', 'channel_id': 'channel_id', 'url_suffix': 'url_suffix', 'long_desc': 'long_desc'}
+
+            title = results[0]['title']
+            duration = results[0]['duration']
+            views = re.sub(r'\D', '', results[0]['views']) # Remove all non-digits from views
+            channel = results[0]['channel']
+            url = content
+            thumbnail = results[0]['thumbnails'][0]
+
+
+        else:   # If it is a Valid YT URL
+
+            with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+                try:
+                    vid_info = ydl.extract_info(query, download=False)
+
+                    title = vid_info['title']
+                    duration = str(round(int(vid_info['duration']) / 60, 3))
+                    views = vid_info['view_count']
+                    channel = vid_info['uploader']
+                    url = vid_info['webpage_url']
+                    thumbnail = vid_info['thumbnail']
+
+                except Exception as e:
+                    log.error(e)
+                    return "vid_not_found"
+
+        # add commas to views
+        views = "{:,}".format(int(views))
+
+        embed.add_field(name="Title", value=f"`{title}`", inline=False)
+        embed.add_field(name="Duration", value=f"`{str(duration).replace('.', ':')}`", inline=False)
+        embed.add_field(name="Views", value=f"`{views}`", inline=False)
+        embed.add_field(name="Channel", value=f"`{channel}`", inline=False)
+        embed.add_field(name="Link", value=f"{url}", inline=False)
+        embed.set_image(url=thumbnail)
+        embed.set_footer(text=embed_footer)
+        await ctx.followup.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(Other(client))
