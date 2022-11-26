c26ffe0458e578e3af63139e507469016b1b946e
Raj Dave <10864@bpsdoha.edu.qa>
2022-11-26 10:32:59 +0300

    some improvements to bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import os
+import sys
 from backend import client, bot_token, log
+import discord.utils
+
 
 @client.event
 async def on_ready():
@@ -11,4 +14,8 @@ for file in os.listdir('./cogs'):
     if file.endswith('.py'):
         client.load_extension(f'cogs.{file[:-3]}')
 
-client.run(bot_token)
+try:
+    client.run(bot_token)
+except discord.LoginFailure:
+    log.critical("Invalid Discord Token. Please check your config file.")
+    sys.exit()
