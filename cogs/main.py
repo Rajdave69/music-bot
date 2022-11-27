ba499334dbc9f4945c6ca1d6e90666138b14041a
Raj Dave <10864@bpsdoha.edu.qa>
2022-11-27 07:56:09 +0300

    add the play command

--- cogs/main.py
+++ cogs/main.py
@@ -28,5 +28,54 @@ class Main(discord.Cog):
     async def on_wavelink_node_ready(self, node: wavelink.Node):
         log.info(f"{node.identifier} is ready.")  # print a message
 
+    @commands.slash_command()
+    async def play(self, ctx, song: str):
+        vc = ctx.voice_client
+        guild = ctx.guild.id
+
+        if guild not in self.queue:
+            self.queue[guild] = []
+
+        if not vc:
+            try:
+                vc = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+            except AttributeError:
+                return await ctx.respond("You are not connected to a voice channel.")
+
+        if ctx.author.voice.channel.id != vc.channel.id:
+            return await ctx.respond("You are not in the same voice channel as me.")
+
+        song = await wavelink.YouTubeTrack.search(song, return_first=True)
+
+        if song.is_stream():
+            return await ctx.respond("Streams are not supported.")
+
+        if song.duration > 600:
+            return await ctx.respond("Songs longer than 10 minutes are not supported.")
+
+        duration = datetime.timedelta(seconds=song.duration)
+
+        embed = discord.Embed(title="Music")
+        embed.add_field(name="Now Playing", value=f"[{song.title}]({song.uri})")
+        embed.add_field(name="Duration", value=f"{str(duration)[2:]}", inline=False)
+        embed.set_image(url=song.thumbnail)
+        embed.set_footer(text=embed_footer)
+
+        if not vc.is_playing():
+            await vc.play(song)
+            embed.add_field(name="Position in Queue", value=f"1", inline=True)
+            await ctx.respond(embed=embed)
+
+        else:
+            if guild not in self.queue:
+                self.queue[guild] = []
+            self.queue[guild].append(song)
+
+            embed.add_field(name="Position in Queue", value=f"{len(self.queue[guild]) + 1}", inline=True)
+            embed.fields[0].name = "Added to Queue"
+
+            await ctx.respond(embed=embed)
+
+
 def setup(client):
     client.add_cog(Main(client))
