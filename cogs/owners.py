5372b1cd93363278fcd5c9f1ef60f3e4f836d755
Raj Dave <10864@bpsdoha.edu.qa>
2022-12-02 01:03:36 +0300

    add basic owners commands

---
+++ cogs/owners.py
@@ -0,0 +1,67 @@
+import sqlite3
+import discord
+from discord.ext import commands
+from backend import log, embed_footer, embed_color, embed_url
+
+
+class Owners(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    owners = discord.SlashCommandGroup("owners", "Owner commands")
+
+    # create a command check
+    def is_owner(self, ctx):
+        return ctx.author.id in [837584356988944396]
+
+    @owners.command()
+    async def reload(self, ctx, cog):
+        if not self.is_owner(ctx):
+            await ctx.respond("You are not the bot owner.")
+            return
+
+        try:
+            self.client.reload_extension(f"cogs.{cog}")
+            await ctx.respond(f"Reloaded {cog}.")
+        except Exception as e:
+            await ctx.respond(f"Failed to reload {cog}.\n{e}")
+
+    @owners.command()
+    async def load(self, ctx, cog):
+        if not self.is_owner(ctx):
+            await ctx.respond("You are not the bot owner.")
+            return
+
+        try:
+            self.client.load_extension(f"cogs.{cog}")
+            await ctx.respond(f"Loaded {cog}.")
+        except Exception as e:
+            await ctx.respond(f"Failed to load {cog}.\n{e}")
+
+    @owners.command()
+    async def exec_sql(self, ctx, sql: str):
+        if not self.is_owner(ctx):
+            await ctx.respond("You are not the bot owner.")
+            return
+
+        con = sqlite3.connect("./data/data.db")
+        cur = con.cursor()
+
+        try:
+            cur.execute(sql)
+            res = cur.fetchall()
+            con.commit()
+
+            embed = discord.Embed(title="SQL", description=f"Executed SQL query.",
+                                  url=embed_url, color=embed_color)
+            for row in res:
+                embed.add_field(name=row, value="‎", inline=False)
+            embed.set_footer(text=embed_footer)
+            await ctx.respond(embed=embed)
+
+        except Exception as e:
+            await ctx.respond(f"Failed to execute SQL.\n{e}")
+
+
+def setup(client):
+    client.add_cog(Owners(client))
